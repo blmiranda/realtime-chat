0ae5fa9036b29b025a491bc94f16e109d1addeb1
Brian Miranda <brianmiranda.dev@gmail.com>
2024-02-18 11:09:57 -0500

    :sparkles: create Title component styles

---
+++ app/src/components/Title/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import theme from '../../global/styles/theme.ts';
+
+export default StyleSheet.create({
+  text: {
+    color: 'white',
+
+    fontFamily: theme.fonts.primary.regular,
+    fontSize: 40,
+    textAlign: 'center',
+  },
+});
