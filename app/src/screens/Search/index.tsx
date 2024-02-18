4b52baeb4d33b4d75e6cb9b0827024ba506b9592
Brian Miranda <brianmiranda.dev@gmail.com>
2024-02-18 11:41:14 -0500

    :sparkles: create prototype version of all the screens

---
+++ app/src/screens/Search/index.tsx
@@ -0,0 +1,12 @@
+import { JSX } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const Search = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Search</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Search;
---
+++ app/src/screens/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import { JSX } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const SignUp = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Sign Up</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
