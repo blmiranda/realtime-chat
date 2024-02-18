a47e45eab7f08beb66a691d1611219129958dabf
Brian Miranda <brianmiranda.dev@gmail.com>
2024-02-18 11:11:18 -0500

    :sparkles: create SplashScreen component styles

---
+++ app/src/screens/SplashScreen/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import theme from '../../global/styles/theme.ts';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
