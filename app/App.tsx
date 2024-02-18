fb918f27f7673e39b7f4c05af975bbed5e480457
Brian Miranda <brianmiranda.dev@gmail.com>
2024-02-18 11:55:33 -0500

    :hammer: implement stack navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,36 @@
 import { JSX } from 'react';
-import SplashScreen from './src/screens/Splash';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Splash from './src/screens/Splash';
+import SignIn from './src/screens/SignIn';
+import SignUp from './src/screens/SignUp';
+import Home from './src/screens/Home';
+import Search from './src/screens/Search';
+import Messages from './src/screens/Messages';
+
+const Stack = createNativeStackNavigator();
 
 const App = (): JSX.Element => {
-  return <SplashScreen />;
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Messages" component={Messages} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ app/src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import { JSX } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const SignIn = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Sign In</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
